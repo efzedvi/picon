330b68715990923a4abbf441db86983f4d19e1c1
Faraz Vahabzadeh <faraz@620>
2021-12-09 23:32:01 -0500

    Simplified the sample code

--- src/main.c
+++ src/main.c
@@ -1,68 +1,48 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
+#include "rtos.h"
 
 #define LED_PIN 25
-#define RED_LED 0
 
-#define GPIO_ON     1
-#define GPIO_OFF    0
-
-void GreenLEDTask(void *param)
+void heartbeat(void *param)
 {
-    gpio_put(LED_PIN, GPIO_ON);
-    vTaskDelay(1000);
-    gpio_put(LED_PIN, GPIO_OFF);
-    vTaskDelay(1000);
+	gpio_put(LED_PIN, 0);
+	printf("off\n");
+	RTOS_TASK_DELAY(1000);
+	gpio_put(LED_PIN, 1);
+	printf("on\n");
+	RTOS_TASK_DELAY(1000);
 }
 
-void RedLEDTask(void *param)
+int main()
 {
-    gpio_put(RED_LED, GPIO_ON);
-    vTaskDelay(100);
-    gpio_put(RED_LED, GPIO_OFF);
-    vTaskDelay(100);
+	stdio_init_all();
+
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+
+	gpio_put(LED_PIN, 1);
+
+	rtos_task_handle_t task_handle = NULL;
+
+	uint32_t status = RTOS_TASK_CREATE(
+		heartbeat,
+		"hrtbt",
+		64,
+		NULL,
+		RTOS_TSK_IDLE_PRIORITY,
+		&task_handle);
+
+	rtos_task_start_scheduler();
+
+	for(;;);
 }
 
-int main() 
-{
-    stdio_init_all();
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    gpio_init(RED_LED);
-    gpio_set_dir(RED_LED, GPIO_OUT);
-
-    TaskHandle_t gLEDtask = NULL;
-    TaskHandle_t rLEDtask = NULL;
-
-    uint32_t status = xTaskCreate(
-                    GreenLEDTask,
-                    "Green LED",
-                    1024,
-                    NULL,
-                    tskIDLE_PRIORITY,
-                    &gLEDtask);
-
-    // status = xTaskCreate(
-    //                 RedLEDTask,
-    //                 "Red LED",
-    //                 1024,
-    //                 NULL,
-    //                 1,
-    //                 &rLEDtask);
-
-    vTaskStartScheduler();
-
-    for( ;; )
-    {
-        //should never get here
-    }
-
-}
